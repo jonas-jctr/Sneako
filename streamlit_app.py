87461cea327399fc352219f32474ca131f908f20
jonas-jctr <124498518+jonas-jctr@users.noreply.github.com>
2025-10-14 15:29:02 +0800

    Sneako_main_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("SNEAKO")
 st.write(
     "FRESH GAME FRESH KICKS"
 )
+
+pages = {
+    "Your Account":[
+        st.Page("my_profile.py", title="My Profile")
+        st.Page("my_listing.py", title="My Listings")
+    ],
+    st.Page("browse.py", title="Browse"
+    st.Page("discounts.py", title="{}sale.format(type_of_sale)"
